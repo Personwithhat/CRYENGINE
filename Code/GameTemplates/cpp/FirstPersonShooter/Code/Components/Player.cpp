f1c0e3420b1bbab279b60007f338f7ec231845d9
VT <personwithhats2@Gmail.com>
2018-08-10 19:23:36 -0700

    Sample FPS template bug fixes
    Perma-turning animation trigger......cuz if mouse = 0 they skip updating the velocity accumulator. Which will continue to add all non-zero values which is stupid xd

--- Code/GameTemplates/cpp/FirstPersonShooter/Code/Components/Player.cpp
+++ Code/GameTemplates/cpp/FirstPersonShooter/Code/Components/Player.cpp
@@ -177,15 +177,15 @@ void CPlayerComponent::UpdateLookDirectionRequest(const CTimeValue& frameTime)
 	const float rotationLimitsMinPitch = -0.84f;
 	const float rotationLimitsMaxPitch = 1.5f;
 
-	if (m_mouseDeltaRotation.IsEquivalent(ZERO, MOUSE_DELTA_TRESHOLD))
-		return;
-
 	// Apply smoothing filter to the mouse input
 	m_mouseDeltaRotation = m_mouseDeltaSmoothingFilter.Push(m_mouseDeltaRotation).Get();
 
 	// Update angular velocity metrics
 	m_horizontalAngularVelocity = (m_mouseDeltaRotation.x * rotationSpeed) / frameTime.BADGetSeconds();
 	m_averagedHorizontalAngularVelocity.Push(m_horizontalAngularVelocity);
+
+	if (m_mouseDeltaRotation.IsEquivalent(ZERO, MOUSE_DELTA_TRESHOLD))
+		return;
 
 	// Start with updating look orientation from the latest input
 	Ang3 ypr = CCamera::CreateAnglesYPR(Matrix33(m_lookOrientation));
