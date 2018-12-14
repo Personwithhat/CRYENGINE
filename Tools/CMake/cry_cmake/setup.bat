cf4aab0a05bc4b4907098928c765f83e868fc017
VT <personwithhats2@Gmail.com>
2018-12-14 14:00:21 -0800

    !FB Fixed and improved cry_cmake.exe
    1) Improved pathing resolution for faster debugging/etc.
    2) Fixed crash because roaming CE directory ain't exist yet -.-
    3) Improved window UI and creation, no longer flashes randomly before centering.
    4) Added GUI option just to re-get CMake setup
    5) Now handles build-failure properly and won't insta-quit or spawn the GUI for no reason.
    6) Improved setup for PyInstall etc.
    
    Plenty more things, all of them improvements.
    
    If I could hide initial console until build that'd be great.
    Just can't figure out how with all this Python/Windows mess.

--- Tools/CMake/cry_cmake/setup.bat
+++ Tools/CMake/cry_cmake/setup.bat
@@ -1,10 +1,20 @@
-REM Tested with Python35
-REM Assumes Python 3.5 installed in c:\Python35
+@echo off
+
+REM Tested with Python37, assumes installed in C:\Python37
+REM Can't use p3 -m PyInstaller because it doesn't pick up changes....
 
 REM PyInstaller must be installed before
 REM To Install PyInstaller run:
-REM  c:\Python35\Scripts\pip.exe install PyInstaller
+REM 	py -3 -m pip install PyInstaller pypiwin32
 
-REM Build CrySelect
-c:\Python35\Scripts\PyInstaller.exe cry_cmake.spec
-copy /Y dist\cry_cmake.exe ..\..\..\cry_cmake.exe
+SET sdir=%~dp0
+
+REM Build cry_cmake.exe
+echo PyInstalling EXE.....
+echo.
+C:\Python37\Scripts\PyInstaller.exe --distpath=%sdir%dist --workpath=%sdir%build %sdir%cry_cmake.spec &&^
+echo. && echo Copying cry_cmake.exe to ENGINE_ROOT..... &&^
+copy /Y %sdir%dist\cry_cmake.exe %sdir%..\..\..\cry_cmake.exe
+
+REM Pause unless there's any arguments.
+IF "%1" == "" ( echo. && PAUSE )
