7f516944e52102882978b36520349972f3fe5fc5
VT <personwithhats2@Gmail.com>
2018-12-23 20:46:59 -0800

    !F Improved cry_select.exe setup
    Matches my other setup script's on download_sdks/cry_cmake

--- Tools/CryVersionSelector/setup.bat
+++ Tools/CryVersionSelector/setup.bat
@@ -1,14 +1,37 @@
-@ECHO OFF
+@echo off
 
-REM tested with Python37
+REM Tested with Python37, assumes installed in C:\Python37
+REM Can't use p3 -m PyInstaller because it doesn't pick up changes....
 
-REM install required modules
-py -3 -m pip install PyInstaller pypiwin32
+REM PyInstaller must be installed before
+REM To Install PyInstaller run:
+REM 	py -3 -m pip install PyInstaller pypiwin32
 
-REM build CrySelect
-py -3 -m PyInstaller cryselect.spec
-copy /Y dist\cryselect.exe cryselect.exe
+SET sdir=%~dp0
 
-REM build CryRun
-py -3 -m PyInstaller cryrun.spec
-copy /Y dist\cryrun.exe cryrun.exe
+REM Build CrySelect
+echo PyInstalling CrySelect EXE.....
+echo.
+C:\Python37\Scripts\PyInstaller.exe --distpath=%sdir%dist --workpath=%sdir%build %sdir%cryselect.spec &&^
+echo. && echo Copying cryselect.exe..... &&^
+copy /Y %sdir%dist\cryselect.exe %sdir%cryselect.exe
+
+REM Pause unless there's any arguments.
+IF "%1" == "" ( echo. && PAUSE )
+
+REM Build CryRun
+echo PyInstalling CryRun EXE.....
+echo.
+py -3 -m PyInstaller --distpath=%sdir%dist --workpath=%sdir%build %sdir%cryrun.spec &&^
+echo. && echo Copying cryrun.exe..... &&^
+copy /Y %sdir%dist\cryrun.exe %sdir%cryrun.exe
+
+REM Pause unless there's any arguments.
+IF "%1" == "" ( echo. && PAUSE )
+
+REM Installing new CrySelect
+echo Installing new CrySelect.
+%sdir%\cryselect.exe install
+
+REM Pause unless there's any arguments.
+IF "%1" == "" ( echo Installed successfully && PAUSE )
