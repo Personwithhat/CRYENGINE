c5c49def65aed3baf5fc9647f8ec5d861cbcf208
VT <personwithhats2@Gmail.com>
2019-01-24 20:05:55 -0800

    !B (CMake) Undefined PathRemoveFileSpecA()
    Possible cause is CMake 3.13.3
    
    Easy fix.

--- Code/Libs/python/python.c
+++ Code/Libs/python/python.c
@@ -2,6 +2,7 @@
 
 #include "Python.h"
 #include "Windows.h"
+#include "shlwapi.h"
 
 int main(int argc, char **argv)
 {
@@ -13,7 +14,7 @@ int main(int argc, char **argv)
 	else
 	{
 		PathRemoveFileSpecA(filename);
-		strcat(filename, "\\..\\..\\Editor\\Python\\windows");
+		strcat_s(filename, RSIZE_MAX, "\\..\\..\\Editor\\Python\\windows");
 		Py_SetPythonHome(filename);
 		return Py_Main(argc, argv);
 	}
